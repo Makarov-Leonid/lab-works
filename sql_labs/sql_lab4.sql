e47fdf1fc90c89c1e1cc17c0c2bf9a42070a480b
Leo Makarov <4.leo.makarov@gmail.com>
2019-11-30 23:52:22 +0300

    ADD TODO [-] Task_1

--- sql_labs/sql_lab4.sql
+++ sql_labs/sql_lab4.sql
@@ -11,21 +11,22 @@
 --
 -- Task_1(Факультет, курс, количество зачетов, количество экзаменов в летнюю сессию)
 
+--TODO: Task_1 не работает корректно, доделать
 
-CREATE OR REPLACE
-VIEW Task_1
-AS SELECT grup_.faculty as faculty,
-          grup_.year as year,
-          case
-            when SESSION.type = 'credit' then 1
-            else 0
-          end as credit,
-          case
-            when SESSION.type = 'exam' and MONTH(SESSION.time_) >= 2 and MONTH(SESSION.time_) <= 8 then 1
-            else 0
-          end as exam
-from SESSION right join grup_ on SESSION.grup_ = grup_.id
-group by faculty, year;
+-- CREATE OR REPLACE
+-- VIEW Task_1
+-- AS SELECT grup_.faculty as faculty,
+--           grup_.year as year,
+--           case
+--             when SESSION.type = 'credit' then 1
+--             else 0
+--           end as credit,
+--           case
+--             when SESSION.type = 'exam' and MONTH(SESSION.time_) >= 2 and MONTH(SESSION.time_) <= 8 then 1
+--             else 0
+--           end as exam
+-- from SESSION right join grup_ on SESSION.grup_ = grup_.id
+-- group by faculty, year;
 
 -- Task_2(Группа, название предмета, даты всех экзаменов и зачетов по этому предмету.)
 
@@ -38,4 +39,22 @@ from SESSION left join CLASSES on SESSION.classes = CLASSES.Name
 order by grup_, name_;
 
 -- Task_3(ФИО преподавателя, название предмета, количество видов контроля по этому предмету для этого преподователя.)
+
+--TODO: Task_3 не работает корректно, доделать
+
+-- CREATE OR REPLACE
+-- VIEW Task_3
+-- AS select SESSION.teacher as teacher,
+--           SESSION.classes as classes,
+--           case
+--              when exists(select * from SESSION where type = 'credit' and SESSION.teacher = teacher) and
+--              exists(select * from SESSION where type = 'exam') then 2
+--              when exists(select * from SESSION where type = 'credit') or
+--              exists(select * from SESSION where type = 'exam') then 1
+--              else 0
+--           end
+-- from (select * from SESSION) as
+-- group by classes
+-- order by teacher, classes;
+
 -- Task_4(Название предмета, кафедра, общее количество студентов, сдающих этот предмет в зимнюю сессию.)
